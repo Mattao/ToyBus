f343b0eb892819be336dcd5d73d3702427844114
matao <muxter.matao@gmail.com>
2018-12-12 15:11:58 +0800

    feat(annotation): define BusReceiver annotation

---
+++ bus/src/main/java/com/matao/bus/annotation/BusReceiver.java
@@ -0,0 +1,21 @@
+package com.matao.bus.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Used for a public non-static method with one parameter and none return value
+ *
+ * <pre>
+ * @BusReceiver
+ * public void onEvent(FooEvent event) {
+ *     // do something...
+ * }
+ * </pre>
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface BusReceiver {
+}
