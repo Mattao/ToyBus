429398023e5096f3f80f55f38f52dc4a701a5266
matao <muxter.matao@gmail.com>
2018-12-13 13:23:27 +0800

    refactor: extract subscriber method invoking into EventEmitter

--- bus/src/main/java/com/matao/bus/Bus.java
+++ bus/src/main/java/com/matao/bus/Bus.java
@@ -2,9 +2,9 @@ package com.matao.bus;
 
 import com.matao.bus.method.MethodInfo;
 import com.matao.bus.method.MethodUtils;
+import com.matao.bus.scheduler.Scheduler;
 import com.matao.bus.scheduler.Schedulers;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -13,7 +13,16 @@ import java.util.Set;
 public class Bus {
 
     private static Bus bus;
+    private Scheduler threadScheduler;
+    private Scheduler mainScheduler;
+    private Scheduler senderScheduler;
     private Map<Object, Set<Subscriber>> subscriberMap = new HashMap<>();
+
+    private Bus() {
+        threadScheduler = Schedulers.thread();
+        mainScheduler = Schedulers.main();
+        senderScheduler = Schedulers.sender();
+    }
 
     public static Bus getDefault() {
         if (bus == null) {
@@ -33,7 +42,7 @@ public class Bus {
             subscriberSet.add(subscriber);
         }
 
-        if (subscriberMap == null || subscriberMap.isEmpty()) return;
+        if (subscriberSet.isEmpty()) return;
         subscriberMap.put(target, subscriberSet);
     }
 
@@ -47,53 +56,24 @@ public class Bus {
             for (final Subscriber subscriber : subscriberSet) {
                 final MethodInfo methodInfo = subscriber.getMethodInfo();
                 if (methodInfo.eventType.isAssignableFrom(eventType)) {
-                    switch (methodInfo.threadMode) {
-                        case Sender:
-                            Schedulers.sender().post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    try {
-                                        subscriber.invoke(event);
-                                    } catch (IllegalAccessException e) {
-                                        e.printStackTrace();
-                                    } catch (InvocationTargetException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            });
-                            break;
-                        case Thread:
-                            Schedulers.thread().post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    try {
-                                        subscriber.invoke(event);
-                                    } catch (IllegalAccessException e) {
-                                        e.printStackTrace();
-                                    } catch (InvocationTargetException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            });
-                            break;
-                        case Main:
-                            Schedulers.main().post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    try {
-                                        subscriber.invoke(event);
-                                    } catch (IllegalAccessException e) {
-                                        e.printStackTrace();
-                                    } catch (InvocationTargetException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            });
-                            break;
-                        default:
-                    }
+                    sendEvent(new EventEmitter(subscriber, event));
                 }
             }
+        }
+    }
+
+    private void sendEvent(EventEmitter emitter) {
+        switch (emitter.threadMode) {
+            case Main:
+                mainScheduler.post(emitter);
+                break;
+            case Thread:
+                threadScheduler.post(emitter);
+                break;
+            case Sender:
+                senderScheduler.post(emitter);
+                break;
+            default:
         }
     }
 }
---
+++ bus/src/main/java/com/matao/bus/EventEmitter.java
@@ -0,0 +1,27 @@
+package com.matao.bus;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class EventEmitter implements Runnable{
+
+    public final Subscriber subscriber;
+    public final Object event;
+    public final ThreadMode threadMode;
+
+    public EventEmitter(Subscriber subscriber, Object event) {
+        this.subscriber = subscriber;
+        this.event = event;
+        this.threadMode = subscriber.getMethodInfo().threadMode;
+    }
+
+    @Override
+    public void run() {
+        try {
+            subscriber.invoke(event);
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
